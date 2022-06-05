b07394c78426fcf5f3646accaa3783b4798dc68a
Pilou-G <pierre.louis.goossens@gmail.com>
2022-06-05 15:10:05 +0200

    Netlify test reset

--- src/components/App.js
+++ src/components/App.js
@@ -80,7 +80,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mm</span>ing</h1>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
           <SearchBar onSearch={this.search} query={this.state.query}/>
           <div className="App-playlist">
